9214b638f094c3a18dd5ca2565f7d824d61d7866
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-11-10 20:10:09 +0200

    db test

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+from fastapi import Depends, FastAPI
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
 from fastapi import FastAPI
 
+DB_USER = os.getenv("MYSQL_USER")
+DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
+DB_HOST = os.getenv("MYSQL_DATABASE")
+DB_NAME = os.getenv("MYSQL_DATABASE_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!!!!!!!!!!"}
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/checkconnection")
+async def read_root(db: Session = Depends(get_db)):
+    return {"message": "Connected to MySQL"}
