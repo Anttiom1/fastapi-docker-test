548a937fbbb1849903e9195b8ae8805617440d74
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-11-10 21:22:14 +0200

    .

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from fastapi import FastAPI
 from sqlalchemy.exc import SQLAlchemyError
+import mysql.connector
 
 
 DB_USER = os.getenv("MYSQL_USER")
